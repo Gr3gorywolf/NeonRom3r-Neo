5548eb5a43eb51cc52d7a933e0d39a468c2e5f8e
Gregory Cabral De Los Santos <Blackarcanjus@gmail.com>
2026-01-21 18:32:41 -0400

    Updated rest api url

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 class AppConstants {
   static String apiBasePath =
       "https://raw.githubusercontent.com/Gr3gorywolf/NeonRom3r-RomsInfos/neo/";
-  static String serverBaseUrl = "http://10.0.0.17:8050/";
+  static String serverBaseUrl = "https://api.yamata-launcher.gregoryc.dev/";
   static String appName = "Yamata Launcher";
   static String baseImagesPath = "assets/images";
   static String appGuid = 'e5a55a91-bdd4-4536-99be-05a564ae1882';
